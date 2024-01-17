8e63b594c92bbab5482ce4887b12566081ec22ba
chizobavictory <chizoba.victory1@gmail.com>
2024-01-17 01:08:03 +0100

    modified detials response

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -130,7 +130,7 @@ const Transactions = () => {
       )}
 
       {uploadResponse && (
-        <div className='mt-4'>
+        <div className='mt-4 overflow-auto'>
           <h2 className='text-lg font-bold mb-2'>Upload Response:</h2>
           <div>{/* Add any additional elements or styling here if needed */}</div>
           <pre className='bg-gray-100 p-4 text-sm rounded-md'>{JSON.stringify(uploadResponse, null, 2)}</pre>
@@ -138,7 +138,7 @@ const Transactions = () => {
       )}
 
       {detailsResponse && (
-        <div className='mt-4'>
+        <div className='mt-4 overflow-auto'>
           <h2 className='text-lg font-bold mb-2'>Full Video Details:</h2>
           <div>{/* Add any additional elements or styling here if needed */}</div>
           <pre className='bg-gray-100 p-4 text-sm rounded-md'>{JSON.stringify(detailsResponse, null, 2)}</pre>
@@ -152,7 +152,7 @@ const Transactions = () => {
           Your browser does not support the video tag.
         </video>
       </div>
-      
+
       <ToastContainer />
     </div>
   );
